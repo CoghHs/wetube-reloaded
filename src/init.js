1cda0c2969174ba8c4e448b56bd8eacf4c2376d4
CoghHs <mrpanda2412@gmail.com>
2023-06-08 02:27:10 +0900

    deleteCommentStart

--- src/init.js
+++ src/init.js
@@ -2,11 +2,12 @@ import "dotenv/config";
 import "./db";
 import "./models/Video";
 import "./models/User";
+import "./models/Comment";
 import app from "./server";
 
 const PORT = 4000;
 
 const handleListening = () =>
- console.log(`✔ Server listenting on port http://localhost:${PORT} ⏳`);
+	console.log(`✔ Server listenting on port http://localhost:${PORT} ⏳`);
 
 app.listen(PORT, handleListening);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,19 +13,27 @@ const PORT = 4002;
 
 const app = express();
 const logger = morgan("dev");
-
+app.use((req, res, next) => {
+	res.header("Cross-Origin-Embedder-Policy", "credentialless");
+	res.header("Cross-Origin-Opener-Policy", "same-origin");
+	next();
+});
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(express.urlencoded( { extended: true } ))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(session({
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({mongoUrl: process.env.DB_URL }),
-}));
+app.use(
+	session({
+		secret: process.env.COOKIE_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+	})
+);
 
+app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
